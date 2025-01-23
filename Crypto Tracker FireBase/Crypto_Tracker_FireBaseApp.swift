73943fe439d04d97daecf3d2a7a2a92410ad8f4b
saibharadwaj135 <saibharadwaj135@gmail.com>
2025-01-23 09:48:49 -0500

    Initial Commit

---
+++ Crypto Tracker FireBase/Crypto_Tracker_FireBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_Tracker_FireBaseApp.swift
+//  Crypto Tracker FireBase
+//
+//  Created by Sai bharadwaj Adapa on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_Tracker_FireBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
